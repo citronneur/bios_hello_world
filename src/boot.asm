d4698120056aa5b6ee561289c80b93ab10e74f5b
citronneur <citronneur@gmail.com>
2015-03-24 22:22:46 +0100

    understand bios load mode

--- src/boot.asm
+++ src/boot.asm
@@ -1,11 +1,16 @@
+bits 16 ; BIOS only accept 16 bits mode
+org 0
+	jmp 07C0h:start
+
 start:
+	mov ax, cs
+	mov ds, ax
+	mov es, ax
 	cli
 	sti
-	mov ah, 0x0e
-	mov al, 'a'
+	mov ah, 0eh
+	mov al, 'b'
 	int 0x10
 
-times 510 - ($ - $$) db 0	; pad to 510 bytes
-dw 0xAA55	; pad 2 more bytes = 512 bytes = THE BOOT SECTOR
-_start:
-	jmp $ ; loop forever
+times 510 - ($ - $$) db 0 ; fill 512 bytes with zero	
+dw 0xAA55 ; boot signature
